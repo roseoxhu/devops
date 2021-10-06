e8e904d7c47111481ebf7e7f855ca6103ef4a643
Roseox <roseox@qq.com>
2021-10-06 18:10:46 +0800

    add firewall utils

---
+++ firewall/ban_ip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+###########################################################
+# Ban the remote IP attack
+# iptables -I INPUT -s 45.121.106.58 -j DROP
+###########################################################
+
+remote_ip=$1
+if [[ "$remote_ip" == "" ]]; then
+    echo "[`date '+%F %T'`] Usage: $0 <remote_ip>"
+    exit 0
+fi
+
+iptables -I INPUT -s $remote_ip -j DROP
+# iptables -nL INPUT --line-numbers | grep "$remote_ip"
+iptables -nL INPUT --line-numbers
